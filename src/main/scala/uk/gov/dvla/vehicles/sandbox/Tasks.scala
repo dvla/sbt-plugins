5dda30ab8cd5a91158026b2e1fc057d3257b6d91
Luchesar Cekov <luchesar.cekov@gmail.com>
2014-11-04 16:48:54 +0000

    Skip running gating tests if there is no gatling simulation defined

--- src/main/scala/uk/gov/dvla/vehicles/sandbox/Tasks.scala
+++ src/main/scala/uk/gov/dvla/vehicles/sandbox/Tasks.scala
@@ -107,36 +107,38 @@ object Tasks {
   val gatlingTestsClassPath = Def.taskDyn {fullClasspath.in(Runtime).in(gatlingTestsProject.value)}
   val gatlingTestsTargetDir = Def.settingDyn {target.in(gatlingTestsProject.value)}
   lazy val testGatling = Def.task {
-
+    val gatlingSimulationClass = gatlingSimulation.value
     def extractVehiclesGatlingJar(toFolder: File) =
       gatlingTestsClassPath.value.find(
         _.data.toURI.toURL.toString.endsWith(s"vehicles-gatling-$VersionVehiclesGatling.jar")
       ).map { jar => IO.unzip(new File(jar.data.toURI.toURL.getFile), toFolder)}
 
-    val targetFolder = gatlingTestsTargetDir.value.getAbsolutePath
-    val vehiclesGatlingExtractDir = new File(s"$targetFolder/gatlingJarExtract")
-    IO.delete(vehiclesGatlingExtractDir)
-    vehiclesGatlingExtractDir.mkdirs()
-    extractVehiclesGatlingJar(vehiclesGatlingExtractDir)
-    System.setProperty("gatling.core.disableCompiler", "true")
-    runProject(
-      gatlingTestsClassPath.value,
-      None,
-      runJavaMain(
-        mainClassName = "io.gatling.app.Gatling",
-        args = Array(
-          "--simulation", gatlingSimulation.value,
-          "--data-folder", s"${vehiclesGatlingExtractDir.getAbsolutePath}/data",
-          "--results-folder", s"$targetFolder/gatling",
-          "--request-bodies-folder", s"$targetFolder/request-bodies"
-        ),
-        method = "runGatling"
-      )
-    ) match {
-      case 0 => println("Gatling execution SUCCESS")
-      case exitCode =>
-        println("Gatling execution FAILURE")
-        throw new Exception(s"Gatling run exited with error $exitCode")
+    if (!gatlingSimulationClass.isEmpty) {
+      val targetFolder = gatlingTestsTargetDir.value.getAbsolutePath
+      val vehiclesGatlingExtractDir = new File(s"$targetFolder/gatlingJarExtract")
+      IO.delete(vehiclesGatlingExtractDir)
+      vehiclesGatlingExtractDir.mkdirs()
+      extractVehiclesGatlingJar(vehiclesGatlingExtractDir)
+      System.setProperty("gatling.core.disableCompiler", "true")
+      runProject(
+        gatlingTestsClassPath.value,
+        None,
+        runJavaMain(
+          mainClassName = "io.gatling.app.Gatling",
+          args = Array(
+            "--simulation", gatlingSimulation.value,
+            "--data-folder", s"${vehiclesGatlingExtractDir.getAbsolutePath}/data",
+            "--results-folder", s"$targetFolder/gatling",
+            "--request-bodies-folder", s"$targetFolder/request-bodies"
+          ),
+          method = "runGatling"
+        )
+      ) match {
+        case 0 => println("Gatling execution SUCCESS")
+        case exitCode =>
+          println("Gatling execution FAILURE")
+          throw new Exception(s"Gatling run exited with error $exitCode")
+      }
     }
   }
 
